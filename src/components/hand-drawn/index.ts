abe790be53a54c82a7201fff62ddbeaa7db3a872
Akshat Thakur <146325203+Akshatthakur22@users.noreply.github.com>
2026-09-24 11:10:52 +0530

    .

--- src/components/hand-drawn/index.ts
+++
@@ -1,7 +0,0 @@
-export { HandDrawnUnderline } from "./HandDrawnUnderline";
-export { HandDrawnArrow } from "./HandDrawnArrow";
-export { MarginNote } from "./MarginNote";
-export { StickyNote } from "./StickyNote";
-export { ConstructionLines } from "./ConstructionLines";
-export { RubberStamp } from "./RubberStamp";
-export { PageDoodles, BinderClip, TapeStrip, Pushpin, TinyStar, SmallArrow } from "./Doodles";
